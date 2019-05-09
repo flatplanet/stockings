cd98585bd7b96e4f0037a1b6f90551ef6f3eaafb
John Elder <johne4196@gmail.com>
2019-05-09 10:58:31 -0700

    installed devise

--- Gemfile
+++ Gemfile
@@ -14,8 +14,10 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
-
-
+gem 'bootstrap', '~> 4.3', '>= 4.3.1'
+gem 'jquery-rails'
+gem 'stock_quote', '~> 2.0'
+gem 'devise', '~> 4.6', '>= 4.6.2'
 
 
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+  	if params[:ticker] == ""
+  		@nothing = "Hey! You Forgot To Enter A Symbol"
+  	elsif params[:ticker]
+  		@stock = StockQuote::Stock.quote(params[:ticker])
+  		if !@stock 
+  			@error = "Hey! That Stock Symbol Doesn't Exist. Please Try Again."
+  		end
+
+  	end
+
   end
+
+  def about
+  end
+
 end
